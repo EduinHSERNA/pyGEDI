a7f3b2856c4a425f40ffe5ba03bea46a0da1bc36
EduinHSERNA <62619290+EduinHSERNA@users.noreply.github.com>
2020-04-05 19:59:40 -0500

    add matplotlib

--- pyGEDI/__init__.py
+++ pyGEDI/__init__.py
@@ -30,6 +30,16 @@ try:
 	import subprocess
 except ImportError:
 	sys.exit("""You need following module: subprocess """)
+	
+try:
+	import matplotlib.pyplot as plt
+except ImportError:
+	sys.exit("""You need following module: matplotlib """)
+	
+try:
+	import copy
+except ImportError:
+	sys.exit("""You need following module: copy """)
 
 class sessionNASA(requests.Session): 
 	AUTH_HOST = 'urs.earthdata.nasa.gov'
@@ -220,3 +230,27 @@ def waveForm(shot_number,fileh5):
     elevation=np.arange(elevation_lastbin_i,elevation_bin0_i,step)[:-1]
     
     return rxwaveform,elevation[::-1]
+
+def histRaster(file_TIFF,bins):
+    gedi_dataset = gdal.Open(file_TIFF)
+    gedi_raster = gedi_dataset.GetRasterBand(1)
+    noDataVal = gedi_raster.GetNoDataValue();print(noDataVal)
+    scaleFactor = gedi_raster.GetScale()    
+    cols = gedi_dataset.RasterXSize; 
+    rows = gedi_dataset.RasterYSize;
+    gedi_array = gedi_dataset.GetRasterBand(1).ReadAsArray(0,0,cols,rows).astype(np.float)
+    gedi_array[gedi_array==int(noDataVal)]=np.nan
+    gedi_array=gedi_array/scaleFactor
+    gedi_nonan_array = copy.copy(gedi_array)
+    gedi_nonan_array = gedi_nonan_array[~np.isnan(gedi_array)]    
+    fig,ax= plt.subplots(figsize=(5,5))
+    plt.hist(gedi_nonan_array.flatten(),bins=bins) 
+    plt.title('Distribution of GEDI points')
+    plt.xlabel('GEDI Tree Height (m)'); plt.ylabel('Number of pixels')
+
+def plotSHP(GEDI_shp,metrics,colormap):
+    GEDI_shp.plot(column = metrics, alpha = 0.9, markersize =5,cmap=colormap,figsize = (5,5))
+    plt.title('GEDI Box', fontsize=15,fontweight='bold')
+
+
+
